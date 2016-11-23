df7100b0d38a0816f3814a08e50327ec0da1187d
Thomas Vimal Easo K <thomasvml@gmail.com>
2016-11-23 20:30:59 +0530

    Added The Main Program
    Execute the file as follow
    
    Syntax :
    (Terminal) $>python tvek_executer.py <your_program_name.py>
    
    Example :
    (Terminal) $>python tvek_executer.py prime.py

---
+++ tvek_executer.py
@@ -0,0 +1,9 @@
+import sys,time,os
+
+prgm_name = sys.argv[1]
+start_time = time.time()
+print("\nProgram Name:"+prgm_name+"\n-------------------------------\nOutput:\n")
+os.system("python "+prgm_name)
+end_time = time.time()
+exec_time = end_time-start_time
+print("\n-------------------------------\nExecution Time : "+str(exec_time)+"\n'"+prgm_name+"' has executed successfully.");
